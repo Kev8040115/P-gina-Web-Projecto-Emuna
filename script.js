f8d53a057e88588e014a79156da56485220922f6
Kev8040115 <kevinfranyerbenitezjimenez@gmail.com>
2026-09-07 11:19:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 //Desplazamiento de la barra de navegación
 
-document.querySelectorAll('a[hreft^="index.html"]').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
-    document.querySelector(this.getAttribute('hreft')). 
-    scrollIntoView({
-        behavior: 'smooth'
-    });  
+        document.querySelector(this.getAttribute('hreft')). 
+        scrollIntoView({
+            behavior: 'smooth'
+        });  
+    });
 });
-});
+
+
+// animación de product hover
+
+const productCards = document.querySelectorAll('.product-card');
+productCards.forEach(card => {
+    card.addEventListener('mouseenter', function() {
+        this.style.transform = 'translateY(-15px)';
+    });
+
+    card.addEventListener('mouseleave', function() {
+        this.style.transform = 'translateY(0)';
+    });
+}); 
